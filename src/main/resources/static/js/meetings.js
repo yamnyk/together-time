61668988872bcce11c5d5c1aa074de016aa9cfa3
yamnyk <notAunicorN2018>
2018-12-28 17:09:17 +0200

    try to fix cancel-btn animation failed

--- src/main/resources/static/js/meetings.js
+++ src/main/resources/static/js/meetings.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     $('.meeting-item__new').hide();
 
     $('.meetings-item__add:last').on('click', (event) => {
-        $('.meetings-item__add').slideUp(100);
-        $('.meeting-item__new').slideDown(100);
+        $('.meetings-item__add').slideUp();
+        $('.meeting-item__new').slideDown();
         $('.meetings-item:last').toggleClass('display-block')
 })
 
     $('.new-cancel').on('click', (event) => {
-        $('.meeting-item__new').slideUp('fast');
+        $('.meeting-item__new').slideUp(2000);
         $('.meetings-item:last').removeClass('display-block');
-        $('.meetings-item__add').slideDown('slow');
+        $('.meetings-item__add').slideDown(5000);
     });
 });
