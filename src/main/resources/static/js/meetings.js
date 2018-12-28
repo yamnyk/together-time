e7c789c90974711cc5d0fc7fd2879d5b67258aa4
yamnyk <notAunicorN2018>
2018-12-28 17:24:41 +0200

    fix cancel-btn animation, but I don't like it

--- src/main/resources/static/js/meetings.js
+++ src/main/resources/static/js/meetings.js
@@ -4,12 +4,10 @@ $(document).ready(function () {
     $('.meetings-item__add:last').on('click', (event) => {
         $('.meetings-item__add').slideUp();
         $('.meeting-item__new').slideDown();
-        $('.meetings-item:last').toggleClass('display-block')
 })
 
     $('.new-cancel').on('click', (event) => {
-        $('.meeting-item__new').slideUp(2000);
-        $('.meetings-item:last').removeClass('display-block');
-        $('.meetings-item__add').slideDown(5000);
+        $('.meeting-item__new').slideUp();
+        $('.meetings-item__add').show(100);
     });
 });
